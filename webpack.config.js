ea9a6496cbb41fd447eb6e9bfc926f9e571aa055
sasha <savylovskij@gmail.com>
2023-04-30 17:23:25 +0300

    add css and scss loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const mode = process.env.NODE_ENV || 'development';
 const devMode = mode === 'development';
@@ -14,16 +15,25 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         clean: true,
-        filename: '[name].[contenthash].js'
+        filename: '[name].[contenthash].js',
     },
-    plugins: [new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, 'src', 'index.html'),
-    })],
+    plugins: [
+        new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'index.html') }),
+        new MiniCssExtractPlugin({ filename: '[name].[contenthash].css'})
+    ],
     module: {
         rules: [
             {
                 test: /\.html$/i,
                 loader: 'html-loader',
+            },
+            {
+                test: /\.(c|sa|sc)ss$/i,
+                use: [
+                    devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ],
             },
         ],
     },
