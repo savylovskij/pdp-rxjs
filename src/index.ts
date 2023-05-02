d744dc47bac2f826db5ab3a05bd9507fdc07ebdb
sasha <savylovskij@gmail.com>
2023-05-02 23:15:16 +0300

    add change js to the ts. Set up config

--- src/index.ts
+++ src/index.ts
@@ -17,38 +17,38 @@ import {
 } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
 
-const INTERVAL = 5000;
+const INTERVAL: number = 5000;
 
 const URL = 'https://random-dog-proxy.onrender.com/api';
 const ACTIVE = 'Active';
 const STOPPED = 'Stopped';
 
-const startEl = document.getElementById('start');
-const stopEl = document.getElementById('stop');
-const imgEl = document.getElementById('image');
-const statusEl = document.querySelector('.status-text');
+const startEl: HTMLElement = document.getElementById(
+  'start'
+) as HTMLButtonElement;
+const stopEl = document.getElementById('stop') as HTMLButtonElement;
+const imgEl = document.getElementById('image') as HTMLImageElement;
+const statusEl = document.querySelector('.status-text') as HTMLElement;
 
-const setImageSrc = src => (imgEl.src = src);
-const setStatusText = text => (statusEl.innerText = text);
+const setImageSrc = (src: string): void => {
+  imgEl.src = src;
+};
+const setStatusText = (text: string): void => {
+  statusEl.innerText = text;
+};
 
 const getImg$ = ajax.getJSON(URL).pipe(
   map(({ url }) => url),
-  filter(url => /.*\.(gif|jpe?g|bmp|png)$/gim.test(url))
+  filter<string>(url => /.*\.(gif|jpe?g|bmp|png)$/gim.test(url))
 );
 
 const stop$ = fromEvent(stopEl, 'click').pipe(
   throttleTime(INTERVAL),
-  tap(() => {
-    startEl.disabled = false;
-    setStatusText(STOPPED);
-  })
+  tap(() => setStatusText(STOPPED))
 );
 
 const start$ = fromEvent(startEl, 'click').pipe(
-  tap(() => {
-    startEl.disabled = true;
-    setStatusText(ACTIVE);
-  }),
+  tap(() => setStatusText(ACTIVE)),
   throttleTime(INTERVAL),
   mergeMap(() =>
     interval(INTERVAL).pipe(
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const mode = process.env.NODE_ENV || 'development';
 const devMode = mode === 'development';
@@ -8,33 +8,43 @@ const devtool = devMode ? 'source-map' : undefined;
 const target = devMode ? 'web' : 'browserslist';
 
 module.exports = {
-    mode,
-    target,
-    devtool,
-    entry: path.resolve(__dirname, 'src', 'index.js'),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-        filename: '[name].[contenthash].js',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'index.html') }),
-        new MiniCssExtractPlugin({ filename: '[name].[contenthash].css'})
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.html$/i,
-                loader: 'html-loader',
-            },
-            {
-                test: /\.(c|sa|sc)ss$/i,
-                use: [
-                    devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader'
-                ],
-            },
+  mode,
+  target,
+  devtool,
+  entry: path.resolve(__dirname, 'src', 'index.ts'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    filename: '[name].[contenthash].js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+    new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(c|sa|sc)ss$/i,
+        use: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
         ],
-    },
-}
+      },
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+};
